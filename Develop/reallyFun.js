92e95b288257de9288cb6d4e74cd4b96ad6357d2
joseph chaudhari <chaudharijoe@gmail.com>
2021-01-27 12:38:11 -0600

    4th commit

--- Develop/reallyFun.js
+++ Develop/reallyFun.js
@@ -1,7 +1,15 @@
 // Display current day upon opening browser
-var date = moment().format('MMMM Do YYYY, h:mm:ss a');
-$(document).ready(function(){
-$("#getDate").click(function(){
-$("button").text("date");
-});
-});
+// referencing moment.js 
+var date = moment().format("MMM Do YY"); 
+// console.log("date:", date);
+$("#currentDay").text(date);
+// color code time blocks to represent past, present, future
+
+
+
+
+// save button saves textbox contents to local storage
+// saved contents remain on page during refresh
+// figure out how to save local content, similar to 04 todos exercise
+
+

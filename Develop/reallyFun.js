87ddcfa99122dcbc775adba7347fc75e08cc637b
joseph chaudhari <chaudharijoe@gmail.com>
2021-01-26 21:44:42 -0600

    3rd commit

--- Develop/reallyFun.js
+++ Develop/reallyFun.js
@@ -1,1 +1,7 @@
 // Display current day upon opening browser
+var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+$(document).ready(function(){
+$("#getDate").click(function(){
+$("button").text("date");
+});
+});
